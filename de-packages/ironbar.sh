ea54e0b5d9051c43da29539a8b761804701b0d04
ditt0132 <104212145+ditt0132@users.noreply.github.com>
2026-06-12 21:19:18 +0900

    Added more required packages

--- de-packages/ironbar.sh
+++ de-packages/ironbar.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-../templates/rust.py ironbar glib2-devel
+../templates/rust.py ironbar \
+  gcc pkgconf-pkg-config \
+  gtk4-devel gtk4-layer-shell-devel glib2-devel \
+  pulseaudio-libs-devel libinput-devel libevdev-devel libxkbcommon-devel dbus-devel \
+  luajit-devel gobject-introspection-devel \
+  gdk-pixbuf2-devel
